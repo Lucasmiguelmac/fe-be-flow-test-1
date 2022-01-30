ca6a3dbb394d0dfce15f4d18f44b975b482c932d
Lucas <lucasmiguelmac@gmail.com>
2022-01-30 12:39:10 +0000

    Fruit app models

--- fruits/models.py
+++ fruits/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Fruit(models.Model):
+    name    = models.CharField(max_length=20)
+    color   = models.ForeignKey('Color', blank=True, null=True, related_name='fruits', on_delete=models.CASCADE)
+
+
+class Color(models.Model):
+
+    class ColorChoices(models.TextChoices):
+        RED     = "Rojo"
+        YELLOW  = "Amarillo"
+        BLUE    = "Azul"
+        ORANGE  = "Naranja"
+        PURPLE  = "Violeta"
+        GREEN   = "Verde"
+
+    name = models.CharField(choices=ColorChoices.choices, max_length=20)
